55a05ef5619a110722d459e60cf11d7246bedf42
SantiiagoSanchez <santiagosanchezz_21@hotmail.com>
2025-01-07 11:51:21 -0300

    Clase entrenador

---
+++ Twenty.BD/Data/Entity/Entrenador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twenty.BD.Data.Entity
+{
+    public class Entrenador
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public int Edad {  get; set; }
+
+        public string Email { get; set; }
+
+        public int Telefono { get; set; }
+
+        public string Actividad { get; set; } //Actividad que dicta (Musculacion, Zumba, Strong, Etc.). Necesita una tabla Aparte
+    }
+}
