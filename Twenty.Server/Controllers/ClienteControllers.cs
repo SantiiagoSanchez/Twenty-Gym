fbe1a655ed0f5a63c16e2ee204b1eb8c30d1a3df
SantiiagoSanchez <santiagosanchezz_21@hotmail.com>
2025-01-27 16:57:33 -0300

    Put Clientes

--- Twenty.Server/Controllers/ClienteControllers.cs
+++ Twenty.Server/Controllers/ClienteControllers.cs
@@ -64,5 +64,48 @@ namespace Twenty.Server.Controllers
                 return BadRequest(e.Message);
             }
         }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Cliente entidad) 
+        {
+            if (id != entidad.Id)
+            {
+                return BadRequest($"No hay un cliente con el ID {id}");
+            }
+
+            var EditarEntity = await context.Clientes.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+            if (EditarEntity == null)
+            {
+                return NotFound("No se logro editar el Cliente");
+            }
+
+            EditarEntity.FotoPerfil = entidad.FotoPerfil;
+            EditarEntity.Nombre = entidad.Nombre;
+            EditarEntity.Documento = entidad.Documento;
+            EditarEntity.Direccion = entidad.Direccion;
+            EditarEntity.Telefono = entidad.Telefono;
+            EditarEntity.Sexo = entidad.Sexo;
+            EditarEntity.Edad = entidad.Edad;
+            EditarEntity.FechaInscripcion = entidad.FechaInscripcion;
+            EditarEntity.Cuota = entidad.Cuota;
+            EditarEntity.Membresia = entidad.Membresia;
+            EditarEntity.Email = entidad.Email;
+            EditarEntity.Alergias = entidad.Alergias;
+            EditarEntity.TelefonoEmergencia = entidad.TelefonoEmergencia;
+            EditarEntity.EntrenadorId = entidad.EntrenadorId;
+            EditarEntity.ActividadId = entidad.ActividadId;
+
+            try
+            {
+                context.Clientes.Update(EditarEntity);
+                await context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }   
     }
 }
